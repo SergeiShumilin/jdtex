3ef42d549c3ec61788ba56abd114925dcdedae9f
SergeiShumilin <NoiSD@yandex.ru>
2018-10-09 13:00:54 +0300

    sss
    Signed-off-by: SergeiShumilin <NoiSD@yandex.ru>

--- plot_graph.py
+++ plot_graph.py
@@ -7,11 +7,15 @@ def plot():
     fig = plt.figure()
     ax1 = fig.add_subplot(121)
     ax2 = fig.add_subplot(122)
-    w = range(2,17)
+    w = np.arange(2,17)
     p = np.arange(0,1,0.01)
     for w in w:
-        ax1.plot(p,func1(p,w))
-        ax2.plot(p,func2(p,w))
+        if w==2:
+            ax1.plot(p, func1(p, w),linewidth=3)
+            ax2.plot(p, func2(p, w),linewidth=3)
+        else:
+            ax1.plot(p,func1(p,w))
+            ax2.plot(p,func2(p,w))
     plt.show()
 
 def func1(p,w):
